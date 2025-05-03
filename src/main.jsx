4d6e7063f6723e2e7f2b7a9ab13bc6853683b905
relbns <ariel@stoketalent.com>
2025-05-03 23:23:03 +0300

    .

--- src/main.jsx
+++ src/main.jsx
@@ -1,15 +1,12 @@
 
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
-import { HashRouter } from 'react-router-dom'; // Import HashRouter
+import { HashRouter } from 'react-router-dom';
 import App from './App.jsx';
-
-// basename is typically not needed with HashRouter
-// const basename = import.meta.env.PROD ? '/otzarot-game/' : '/';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <HashRouter> {/* Use HashRouter */}
+    <HashRouter>
       <App />
     </HashRouter>
   </StrictMode>,
